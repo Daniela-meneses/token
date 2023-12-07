a8993f7948477081189b2a38fe78c804b1147032
danys <122809726+Daniela-meneses@users.noreply.github.com>
2023-12-07 16:26:00 +0000

    se actualizo

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends
 from fastapi.security import HTTPBasicCredentials, HTTPBearer, HTTPAuthorizationCredentials
 from fastapi.security import HTTPBasic
 from fastapi.responses import JSONResponse
@@ -23,6 +23,41 @@ async def db_middleware(request, call_next, db: sqlite3.Connection = Depends(get
     request.state.db = db
     response = await call_next(request)
     return response 
+
+@app.post("/registro")
+async def registro(db: sqlite3.Connection = Depends(get_db), datos_usuario: dict = Depends()):
+    cursor = db.cursor()
+
+    query = 'INSERT INTO usuario (email, password1, token, timestamp1, estado) VALUES (?, ?, ?, ?, ?)'
+    hashed_password = hashlib.md5(datos_usuario.get("password").encode()).hexdigest()
+    valores = (datos_usuario.get("email"), hashed_password, secrets.token_urlsafe(12), str(datetime.now()), "activo")
+    
+    cursor.execute(query, valores)
+    db.commit()
+
+    return {"message": "Registro exitoso"}
+
+@app.post("/login")
+async def login(credentials: HTTPBasicCredentials = Depends(), db: sqlite3.Connection = Depends(get_db)):
+    email = credentials.username
+    password_hash = hashlib.md5(credentials.password.encode()).hexdigest()
+
+    cursor = db.cursor()
+    query = "SELECT * FROM usuario WHERE email = ? AND password1 = ?"
+    cursor.execute(query, (email, password_hash))
+    user = cursor.fetchone()
+
+    if user:
+        new_token = secrets.token_urlsafe(12)
+        timestamp = str(datetime.now())
+
+        update_query = "UPDATE usuario SET token = ?, timestamp1 = ? WHERE email = ?"
+        cursor.execute(update_query, (new_token, timestamp, email))
+        db.commit()
+
+        return {"auth": True, "token": new_token}
+    else:
+        {"auth": False, "detail": "Invalid credentials"}
 
 @app.get("/")
 def index(credentials: HTTPBasicCredentials = Depends(security_basic), db: sqlite3.Connection = Depends(get_db)):
